81c1438c96089373ffbbdb58eb3d3c6117e0276b
Krzysztof Ługowski <k.lugowski@yahoo.com>
2018-02-05 20:45:35 +0100

    Add files

---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+
+
+const Title = props => <h1>{props.title}</h1>
+
+const one = "hello";
+    const two = "world"
+    console.log(`${one} ${two}`);
+
+
---
+++ src/components/Todo.js
@@ -0,0 +1,5 @@
+const Title = props => <h1>{props.title}</h1>
+
+const one = "hello";
+    const two = "Todo"
+    console.log(`${one} ${two}`);
