846aa454049386759fa3950bb55ea6f426e4f3a3
Pawel Furman <pf.furman@gmail.com>
2018-02-06 20:51:59 +0100

    Split to small components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,68 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
-import Title from '../components/Title.js';
-import Todo from '../components/Todo.js';
+
+class TodoTitle extends React.Component{
+    render(){
+        return <h2>{this.props.content}</h2>
+    }
+}
+
+class Todo extends React.Component{
+    removeItem(){
+        this.props.removeAction(this.props.id);
+    }
+    render(){
+        return(
+            <div className={style.Todo}>
+                {this.props.text}
+                <button type="button" onClick={this.removeItem.bind(this)}>Remove Todo</button>
+            </div>
+        )
+    }
+}
+
+
+class TodoList extends React.Component{
+    onRemove(id){
+        this.props.remove(id);
+    }
+
+    render(){
+        return(
+            <ul>
+                {this.props.items.map(item => {
+                    return (<Todo key={item.id} text={item.text} id={item.id} removeAction={this.onRemove.bind(this)} />)
+                })}
+            </ul>
+        );  
+    }
+}
+
+
+
+
+class TodoForm extends React.Component{
+        
+    changeInput(ev){
+        this.props.onChangeAction(ev.target.value);
+    }
+
+
+    addItem(){
+        this.props.onAddTodoAction();
+    }
+    
+
+    render(){
+        return (
+            <form>
+                <input type="text" onChange={this.changeInput.bind(this)} />
+                <button type="button" onClick={this.addItem.bind(this)}>Add Todo</button>
+            </form>
+        )
+    }
+}
 
 
 class App extends React.Component {
@@ -28,26 +88,19 @@ class App extends React.Component {
         this.setState({todos: remainder});
     }
 
-    handleOnChange(ev){
-        this.setState({currentTodo: ev.target.value})
+    handleOnChange(value){
+        this.setState({currentTodo: value})
     }
 
 
     render() {
       return (
         <div className={style.TodoApp}>
-            <form>
-                <input type="text" onChange={this.handleOnChange.bind(this)} />
-                <button type="button" onClick={this.addTodo.bind(this)}>Add Todo</button>
-            </form>
-            <ul>
-                {this.state.todos.map(todo => {
-                    return (<div key={todo.id}>
-                            {todo.text}
-                            <button type="button" onClick={this.removeTodo.bind(this, todo.id)}>Remove Todo</button>
-                        </div>)
-                })}
-            </ul>
+            <TodoTitle content="Lista rzeczy do zrobienia" />
+            <TodoForm onChangeAction={this.handleOnChange.bind(this)} onAddTodoAction={this.addTodo.bind(this)} />
+
+
+            <TodoList items={this.state.todos} remove={this.removeTodo.bind(this)} />
         </div>
       );
     }
